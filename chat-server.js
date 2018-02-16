bf5254bd15f355ba4021d1246160302fa9e7c1a9
yshin4 <yshin4@lion.lmu.edu>
2018-02-16 15:06:39 -0800

    added server

--- chat-server.js
+++ chat-server.js
@@ -3,3 +3,9 @@ var sockets = [];
 var port = 9633;
 
 console.log("runniing");
+
+var server = net.createServer(function(socket) {
+	console.log("The server is running");
+	socket.write("Welcome to the GungHo test chat server\nLogin name?");
+	socket.write("Login name?");
+}) 
