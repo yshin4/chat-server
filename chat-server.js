6133509a6dbf87a3b03a040942f892ec20fc32bf
yshin4 <yshin4@lion.lmu.edu>
2018-02-16 15:09:33 -0800

    server listen, error

--- chat-server.js
+++ chat-server.js
@@ -5,7 +5,15 @@ var port = 9633;
 console.log("runniing");
 
 var server = net.createServer(function(socket) {
-	console.log("The server is running");
-	socket.write("Welcome to the GungHo test chat server\nLogin name?");
-	socket.write("Login name?");
-}) 
+    console.log("The server is running");
+    socket.write("Welcome to the GungHo test chat server\nLogin name?");
+    socket.write("Login name?");
+});
+
+server.on("error", function(error) {
+    console.log("Error: ", error.message);
+});
+
+server.listen(port, function() {
+    console.log("Server listening at http://localhost:", port);
+});
